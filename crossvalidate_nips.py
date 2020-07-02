44eba48a6253ed0938033a5ee943e9725a96028c
fredwang25 <fredwang25@gmail.com>
2020-07-02 15:41:08 -0700

    bug fix

--- crossvalidate_nips.py
+++ crossvalidate_nips.py
@@ -36,10 +36,11 @@ for skill in range(skill_count):
             best_model = fitmodel
     
     (correct_predictions, state_predictions) = predict_onestep.run(best_model, test_data[skill])
-    curr_auc = auc.compute_auc(test_data[skill]["data"], correct_predictions, False)
-    print("Skill %s of %s calculation completed with AUC of %.4f" % (skill, skill_count, curr_auc))
-    total_auc += curr_auc * len(test_data[skill]["resources"])
-    total_trials += len(test_data[skill]["resources"])
+    if len(set(test_data[skill]["data"]) > 1):#auc only calculated when there are 2+ classifiers
+        curr_auc = auc.compute_auc(test_data[skill]["data"], correct_predictions, False)
+        print("Skill %s of %s calculation completed with AUC of %.4f" % (skill, skill_count, curr_auc))
+        total_auc += curr_auc * len(test_data[skill]["resources"])
+        total_trials += len(test_data[skill]["resources"])
 total_auc /= total_trials
 print("Overall AUC:", total_auc)
 #specifying verbose allows data from all iterations of crossvalidation to be printed out
