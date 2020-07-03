ddbcf32d4f6c1d58c4a0d159f814ae687c130a4c
Frederic <fredwang25@gmail.com>
2020-07-02 17:33:23 -0700

    bug fixes

--- crossvalidate_nips.py
+++ crossvalidate_nips.py
@@ -27,20 +27,25 @@ total_trials = 0
 for skill in range(skill_count):
     num_fit_initializations = 5
     best_likelihood = float("-inf")
-    
-    for i in range(num_fit_initializations):
-        fitmodel = random_model_uni.random_model_uni(1, 1)
-        (fitmodel, log_likelihoods) = EM_fit.EM_fit(fitmodel, Data[skill])
-        if(log_likelihoods[-1] > best_likelihood):
-            best_likelihood = log_likelihoods[-1]
-            best_model = fitmodel
-    
-    (correct_predictions, state_predictions) = predict_onestep.run(best_model, test_data[skill])
-    if len(np.unique(test_data[skill]["data"]) > 1):#auc only calculated when there are 2+ classifiers
-        curr_auc = auc.compute_auc(test_data[skill]["data"], correct_predictions, False)
-        print("Skill %s of %s calculation completed with AUC of %.4f" % (skill, skill_count, curr_auc))
-        total_auc += curr_auc * len(test_data[skill]["resources"])
-        total_trials += len(test_data[skill]["resources"])
+    if len(Data[skill]["resources"]) < 1:
+        print("No data for skill %s" % skill)
+        continue
+    else:
+        for i in range(num_fit_initializations):
+            fitmodel = random_model_uni.random_model_uni(1, 1)
+            (fitmodel, log_likelihoods) = EM_fit.EM_fit(fitmodel, Data[skill])
+            if(log_likelihoods[-1] > best_likelihood):
+                best_likelihood = log_likelihoods[-1]
+                best_model = fitmodel
+        
+        if len(test_data[skill]["resources"]) > 1 and len(np.unique(test_data[skill]["data"])) > 1:#auc only calculated when there are 2+ classifiers
+            (correct_predictions, state_predictions) = predict_onestep.run(best_model, test_data[skill])
+            curr_auc = auc.compute_auc(test_data[skill]["data"], correct_predictions, False)
+            print("Skill %s of %s calculation completed with AUC of %.4f" % (skill, skill_count, curr_auc))
+            total_auc += curr_auc * len(test_data[skill]["resources"])
+            total_trials += len(test_data[skill]["resources"])
+        else:
+            print("No test data for skill %s" % skill)
 total_auc /= total_trials
 print("Overall AUC:", total_auc)
 #specifying verbose allows data from all iterations of crossvalidation to be printed out
